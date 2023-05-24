d728dd77e6ff13fd3b9f4c94324fd890b6dc5454
Diego Masin <diegoifce@gmail.com>
2023-05-24 09:58:15 -0300

    First post views

--- backend/cursos/views.py
+++ backend/cursos/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -16,6 +17,18 @@ class CourseAPIView(APIView):
 
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
 
 class RatingAPIView(APIView):
     """
@@ -27,3 +40,13 @@ class RatingAPIView(APIView):
         serializer = RatingSerializer(ratings, many=True)
 
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RatingSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
